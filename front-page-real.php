5b943324b58fc056ddf61c875e94173af8a6ff6b
jingxin <benxiaohaizi007@sina.com>
2012-10-19 00:32:11 +0800

    xin.jingx

---
+++ front-page-real.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php bloginfo( 'name' ); ?></title>
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri().'/style/css/common/layout.css'; ?>" type="text/css" />
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri().'/style/css/page/index.css'; ?>" type="text/css" />
+		<script src="<?php echo get_template_directory_uri().'/style/js/common/jquery-1.8.js'; ?>"></script>
+		<script src="<?php echo get_template_directory_uri().'/style/js/common/jcarousel.js'; ?>"></script>
+	</head>
+	
+	<body>
+		<div class="mod">
+			<div class="mod-content">
+				<div id="header" class="clr">
+					<div class="logo">
+						<a href="http://127.0.0.1/wordpress">
+							<img alt="cstar" src="http://127.0.0.1/wordpress/wp-content/themes/orange/style/img/common/logo.gif" />
+						</a>
+					</div>
+					<div class="navigation">
+						<!-- <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?> -->
+						<div class="menu-juzi-container">
+							<ul class="menu" id="menu-juzi">
+								<li class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-13 first_item" id="menu-item-13"><a href="http://127.0.0.1/wordpress/"><span class="menu-link-left"></span><span class="menu-link">Home</span><span class="menu-link-right"></span></a></li>
+								<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-14 last_item" id="menu-item-14" style="background:none!important;height:22px!important;"><a href="http://google.com"><span class="menu-link-left"></span><span class="menu-link">Company</span><span class="menu-link-right"></span></a></li>
+								<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-14 last_item" id="menu-item-14" style="background:none!important;height:22px!important;"><a href="http://google.com"><span class="menu-link-left"></span><span class="menu-link">Products</span><span class="menu-link-right"></span></a></li>
+								<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-14 last_item" id="menu-item-14" style="background:none!important;height:22px!important;"><a href="http://google.com"><span class="menu-link-left"></span><span class="menu-link">Motif Design</span><span class="menu-link-right"></span></a></li>
+								<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-14 last_item" id="menu-item-14" style="background:none!important;height:22px!important;"><a href="http://google.com"><span class="menu-link-left"></span><span class="menu-link">How To Apply</span><span class="menu-link-right"></span></a></li>
+								<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-14 last_item" id="menu-item-14" style="background:none!important;height:22px!important;"><a href="http://google.com"><span class="menu-link-left"></span><span class="menu-link">Contact Us</span><span class="menu-link-right"></span></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="clare">
+				<div class="division"></div>
+				<h2>Here is some infomation about holiday or other special info.</h2>
+				<div class="division"></div>
+			</div>
+			<div class="mod-content">
+				<div class="carousel-v1">
+					<ul id="mycarousel">
+						<li><a href="#"><img src="http://www.cstarintl.com/images/1.jpg" /></a></li>
+						<li><a href="#"><img src="http://www.cstarintl.com/images/2.jpg" /></a></li>
+						<li><a href="#"><img src="http://www.cstarintl.com/images/3.jpg" /></a></li>
+						<li><a href="#"><img src="http://www.cstarintl.com/images/4.jpg" /></a></li>
+						<li><a href="#"><img src="http://www.cstarintl.com/images/5.jpg" /></a></li>
+						<li><a href="#"><img src="http://www.cstarintl.com/images/6.jpg" /></a></li>
+					</ul>
+				</div>
+				<h3 class="commontitle">Hot Products</h3>
+				<div class="division" style="margin:10px 0 25px 0;"></div>
+				<ul class="showlist1 clr">
+					<li><a href="#"><span>type1</span><img src="http://www.swasky.com/images/tu11_58.gif" /></a></li>
+					<li><a href="#"><span>type2type2type2type2</span><img src="http://www.swasky.com/images/tu12_58.gif" /></a></li>
+					<li><a href="#"><span>type3type3</span><img src="http://www.cstarintl.com/images/3.jpg" /></a></li>
+					<li><a href="#"><span>type4</span><img src="http://www.cstarintl.com/images/4.jpg" /></a></li>
+					<li><a href="#"><span>type5</span><img src="http://www.cstarintl.com/images/4.jpg" /></a></li>
+					<li><a href="#"><span>type6</span><img src="http://www.cstarintl.com/images/4.jpg" /></a></li>
+					<li><a href="#"><span>type7</span><img src="http://www.cstarintl.com/images/4.jpg" /></a></li>
+					<li><a href="#"><span>type8</span><img src="http://www.cstarintl.com/images/4.jpg" /></a></li>
+				</ul>
+			</div>
+			<div class="showblock">
+				<div class="mod-content">
+					<h3 class="commontitle">New Designs Show Case</h3>
+					<ul class="showlist clr">
+						<li><a href="#"><img src="http://www.cstarintl.com/images/1.jpg" /></a></li>
+						<li><a href="#"><img src="http://www.cstarintl.com/images/2.jpg" /></a></li>
+						<li><a href="#"><img src="http://www.cstarintl.com/images/3.jpg" /></a></li>
+						<li><a href="#"><img src="http://www.cstarintl.com/images/4.jpg" /></a></li>
+						<li><a href="#"><img src="http://www.cstarintl.com/images/5.jpg" /></a></li>
+						<li><a href="#"><img src="http://www.cstarintl.com/images/6.jpg" /></a></li>
+						<li><a href="#"><img src="http://www.cstarintl.com/images/1.jpg" /></a></li>
+						<li><a href="#"><img src="http://www.cstarintl.com/images/2.jpg" /></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="mod-content">
+				<div id="footer">
+					<h3 class="commontitle contacttitle">Contact Us</h3>
+					<div class="division" style="margin:20px 0 35px 0;"></div>
+					<div class="contactlist clr">
+						<ul>
+							<li><h3>CHINA</h3></li>
+							<li><b>TEL</b><span>86-571-85333051</span></li>
+							<li><b>FAX</b><span>86-571-85333052</span></li>
+							<li><b>E-MAIL</b><span>info@cstarintl.com</span></li>
+							<li><b>MSN</b><span>swasky@swasky.com</span></li>
+							<li><b>HOURS</b><br/><span>Monday-Saturday 9:00 a.m-6:00 p.m China Time</span></li>
+							<li><b>OFFICE ADDRESS(in China)</b><br/><span>15/F,Dongguan Building ,No.4288 Jiangnan Road,Binjiang District,Hangzhou, China</span></li>
+						</ul>
+						<div class="division2"></div>
+						<ul>
+							<li><h3>U.S.A</h3></li>
+							<li><b>TEL</b><span>86-571-85333051</span></li>
+							<li><b>FAX</b><span>86-571-85333052</span></li>
+							<li><b>E-MAIL</b><span>info@cstarintl.com</span></li>
+							<li><b>MSN</b><span>swasky@swasky.com</span></li>
+							<li><b>HOURS</b><br/><span>Monday-Saturday 9:00 a.m-6:00 p.m China Time</span></li>
+							<li><b>OFFICE ADDRESS(in China)</b><br/><span>15/F,Dongguan Building ,No.4288 Jiangnan Road,Binjiang District,Hangzhou, China</span></li>
+						</ul>
+					</div>	
+					<div class="supportpic"></div>
+				</div>
+			</div>
+		</div>
+		<script type="text/javascript">
+			jQuery(document).ready(function() {
+    			jQuery('#mycarousel').jcarousel({
+    				scroll:1,
+    				auto:3,
+    				animation:1000,
+    				wrap:'circular'
+    			});
+			});
+		</script>
+
+	</body>
+</html>
